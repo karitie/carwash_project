0b5f65af3ac1744e6094ee50b7eb98aa2137abff
karitie <karitiesamuel7@gmail.com>
2025-12-11 05:13:46 +0300

    Second commit

--- carwash/settings.py
+++ carwash/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load environment variables from .env file
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,7 +143,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'karitiesamuel7@gmail.com'
-EMAIL_HOST_PASSWORD = 'enaq gfaa puas uwws'
+
 
 
